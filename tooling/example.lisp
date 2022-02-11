7d4b6448188008ce49c4c3ecd0244c787c9e02df
porcuquine <porcuquine@users.noreply.github.com>
2022-02-11 08:47:49 -0800

    Disable Lurk submodules and example tests on CI.

--- tooling/example.lisp
+++ tooling/example.lisp
@@ -30,19 +30,21 @@
             "~A" (get-output-stream-string out))))))
 
 (test examples-internally
-  (let* ((root-dir (or *project-dir* (uiop/os:getcwd)))
-         (example-dir (merge-pathnames "lurk-lib/example/" root-dir)))
-    (dolist (test-file *example-tests*)
-      (let ((merged (merge-pathnames test-file example-dir)))
-        (dolist (type *repl-types-to-test*)
-          (let ((out (make-string-output-stream)))
-            (multiple-value-bind (repl state)
-                (repl:make-repl-and-state :type type :out out)
-              (is (not (null
-                        (handler-case
-                            ;; Return T if Lurk file is run successfully.
-                            (prog1 t
-                              (repl:run repl state merged))
-                          ;; Return NIL if an error is signaled while running.
-                          (error () nil))))
-                  "~A" (get-output-stream-string out)))))))))
+  (unless  (uiop:getenv "ON_CI_SERVER")
+    (let* ((root-dir (or *project-dir* (uiop/os:getcwd)))
+           (example-dir (merge-pathnames "lurk-lib/example/" root-dir)))
+      (dolist (test-file *example-tests*)
+        (let ((merged (merge-pathnames test-file example-dir)))
+          (dolist (type *repl-types-to-test*)
+            (let ((out (make-string-output-stream)))
+              (multiple-value-bind (repl state)
+                  (repl:make-repl-and-state :type type :out out)
+                (is (not (null
+                          (handler-case
+                              ;; Return T if Lurk file is run successfully.
+                              (prog1 t
+                                (repl:run repl state merged))
+                            ;; Return NIL if an error is signaled while running.
+                            (error () nil))))
+                    "~A" (get-output-stream-string out))))))))))
+
